283ff394e78f9250cbcc61530f11c56c2b670bf9
George V <georgevuong18@gmail.com>
2017-09-19 20:40:40 -0700

    setup githubServer to make request to github api

--- node/github-grabber/animal_fun.js
+++ node/github-grabber/animal_fun.js
@@ -1,27 +1,28 @@
 const fs = require('fs');
 const http = require('http');
+const querystring = require('querystring');
 
+// create a HTTP server object
 const server = http.createServer((req, res) => {
-  res.write("Hello World");
-  res.end();
-})
-
-server.listen(8000, () =>
-  console.log("Listening on port 8000");
-)
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("Hello World!"); // write response to client
+  res.end(); // end response
+}).listen(8000, () => console.log(listening on 8000);); // server object listens on port 8000
 
 // fs.readFile is an async fcn. Callback provides an error obj (if it exists) and data from file. This order of callback is common in Node.
-// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(data);
-//   }
-// })
+fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    // console.log(data);
+    console.log("file is successfully read!");
+  }
+})
 
 // sync version of readFile()
 // let animal_text = fs.readFileSync('./animals.txt', 'utf-8')
 // console.log(animal_text)
+
 
 // returns a buffer object if encoding type is not stated, Buffers are instances of Buffer class in node, designed to handle raw binary data. Each buffer corresponds to raw memory allocated outside V8.
 // fs.readFile('./animals.txt', (err, data) => {
---
+++ node/github-grabber/github_grabber.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const querystring = require('querystring');
+const https = require('https');
+const http = require('http');
+
+// create HTTP server object
+const githubServer = http.createServer((req, res) => {
+  if (req == 'POST') {
+    res.end("I'm a POST request!");
+  } else {
+    res.end("Not a POST request!");
+  }
+})
+
+githubServer.listen(8080, () =>
+  console.log('Listening on 8080')
+); // server object listens on port 8080
