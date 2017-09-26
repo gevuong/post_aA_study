d7489fc3ccedee7ce261adc8d67ce3eb7da3f919
George V <georgevuong18@gmail.com>
2017-09-26 16:05:36 -0700

    connect socket.io server with main server

--- node/chat_app/public/javascripts/index.js
+++ node/chat_app/public/javascripts/index.js
@@ -1,15 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
   const socket = require('socket.io-client')() // invoked with ()
-  console.log(socket);
-  // const ChatUI = require('./chatUI')
-  // const myChat = new ChatUI(socket)
+  // console.log(socket);
 })
-// const express = require('../../node_modules/express');
-// // const app = express();
-// // const fs = require('fs');
-// // const server = require('http').createServer(app);
-// const io = require('socket.io')(server); // need to pass http Server instance to socket.io
-// // io.on('connection', function() {
-// //
-// // });
-// // server.listen(4000); // call listen on server, not the app.
