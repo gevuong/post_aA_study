61cc184b8e62558f45a1e8a62e01accaf07a17a8
George V <georgevuong18@gmail.com>
2017-09-27 19:11:14 -0700

    setup DOM selection and styling

--- frontend/DOM/app.js
+++ frontend/DOM/app.js
@@ -1,4 +1,14 @@
-const myHeading = document.getElementById("myHeading");
+// getElementById method belongs to the document object
+const myHeading = document.getElementsByTagName("h1")[0];
+const myButton = document.getElementById("myButton");
+const resetHeading = document.getElementById("resetHeading")
+const myTextInput = document.getElementById("myTextInput");
+const myBackgroundInput = document.getElementById("myBackgroundInput");
+const buttonBackground = document.getElementById('buttonBackground')
+const listItems = document.querySelectorAll('li');
+const nonPurpleItems = document.getElementsByClassName('not-purple');
+// li:nth-child is a CSS pseudo class selector
+const evens = document.querySelectorAll('li:nth-child(even)');
 
 myHeading.addEventListener('click', function() {
   if (myHeading.style.color === 'red') {
@@ -7,3 +17,26 @@ myHeading.addEventListener('click', function() {
     myHeading.style.color = 'red';
   }
 });
+
+resetHeading.addEventListener('click', () => {
+  myHeading.style.color = 'black';
+})
+
+myButton.addEventListener('click', () => {
+  myHeading.style.color = myTextInput.value;
+})
+
+buttonBackground.addEventListener('click', () => {
+  myHeading.style.backgroundColor = myBackgroundInput.value;
+})
+
+for (let item = 0; item < listItems.length; item++) {
+  listItems[item].style.color = 'purple';
+}
+
+for (let item = 0; item < nonPurpleItems.length; item++) {
+  nonPurpleItems[item].style.color = 'orange';
+}
+for (let item = 0; item < evens.length; item++) {
+  evens[item].style.backgroundColor = 'lightgray';
+}
